bd2c4c0fb7203766332ba9dd714ca0f8eb9033ba
MattCrmx <matthias.cremieux@hotmail.fr>
2024-01-25 20:02:51 +0100

    chg: :sparkles: add basic parsing methods

--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,5 @@
+use std::result;
+
 struct HTMLParser {
     position: usize,
     input: String,
@@ -10,6 +12,7 @@ impl HTMLParser {
 
     pub fn get_next_char(&self) -> Option<char> {
         // return None if we reach the end of the word
+        // doesn't consume the char
         if !self.eol() {
             Some(self.input.chars().nth(self.position + 1).unwrap())
         } else {
@@ -26,7 +29,35 @@ impl HTMLParser {
         }
     }
 
-    pub fn consume(&mut self) -> char {
+    pub fn consume_char(&mut self) -> char {
+        if self.eol() {
+            panic!("Trying to consume character when end of input is reached");
+        }
+
+        let mut char_iter = self.input[self.position..].chars();
+        self.position += 1;
         
+        char_iter.next().unwrap()
+    }
+
+    pub fn consume_chars_while<F>(&mut self, predicate: F) -> String 
+        where F: Fn(char) -> bool {
+            let mut result = String::from("");
+            let mut cons: bool = true;
+
+            // loop through the input
+            while !self.eol()  && cons {
+                let next_char = self.get_next_char();
+                cons = match next_char {
+                    Some(c) => predicate(c),
+                    None => false
+                };
+                result.push(self.consume_char());
+            }
+            return result
+        }
+
+    pub fn remove_whitespaces(&mut self) {
+        self.consume_chars_while(|c| c.is_whitespace());
     }
 }
